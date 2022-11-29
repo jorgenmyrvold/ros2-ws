3b6e8ae6a62b70c10e63ae98c9c5532a661b0b17
Jørgen Myrvold <myrvoldou@gmail.com>
2022-11-29 13:16:14 +0100

    Refactored and modularized urdfs

--- src/kmr_description/launch/display.launch.py
+++ src/kmr_description/launch/display.launch.py
@@ -10,9 +10,11 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
     urdf_tutorial_path = get_package_share_path('kmr_description')
-    default_model_path = urdf_tutorial_path / 'urdf/robot/kmriiwa.urdf.xacro'
-    # default_model_path = urdf_tutorial_path / 'urdf/gripper/robotiq_gripper.urdf.xacro'
-    # default_model_path = urdf_tutorial_path / 'urdf/d435/_d435.urdf.xacro'
+    default_model_path = urdf_tutorial_path / 'urdf/robot/kmr.xacro'
+    # default_model_path = urdf_tutorial_path / 'urdf/gripper/robotiq_gripper_standalone.xacro'
+    # default_model_path = urdf_tutorial_path / 'urdf/d435/_d435_standalone.xacro'
+    # default_model_path = urdf_tutorial_path / 'urdf/iiwa14/iiwa14_standalone.xacro'
+    # default_model_path = urdf_tutorial_path / 'urdf/kmp200/kmp200_standalone.xacro'
     default_rviz_config_path = urdf_tutorial_path / 'rviz/urdf.rviz'
 
     gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
