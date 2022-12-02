c6fd80f7bacada33e9d76b178033bea2feaa3ef3
Jørgen Myrvold <jorgeum@stud.ntnu.no>
2022-12-02 16:59:02 +0100

    Added boilerplate to  kmr_moveit2.launch.py

---
+++ src/kmr_moveit2/launch/kmr_moveit2.launch.py
@@ -0,0 +1,27 @@
+import os
+import yaml
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return file.read()
+    except EnvironmentError:
+        return None
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return yaml.load(file)
+    except EnvironmentError:
+        return None
