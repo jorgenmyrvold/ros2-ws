2dab1e20c5804a23e0e4245dc16a2802aef95af7
Jørgen Myrvold <myrvoldou@gmail.com>
2022-12-07 15:44:35 +0100

    Started implementing kmr_bringup package. Somewhat working

---
+++ src/kmr_bringup/kmr_bringup/kmr_bringup.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from kmr_bringup.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/kmr_bringup/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'kmr_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # ('share/' + package_name, ['bringup.launch.py'])
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name), glob('rviz/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jorgen',
+    maintainer_email='myrvoldou@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'kmr_bringup = kmr_bringup.kmr_bringup:main',
+        ],
+    },
+)
